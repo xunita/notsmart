cd18d17a0f9ed5e6f2621cb1a6ed42a2cf196893
xunita <yayasopeguesoro@gmail.com>
2025-04-16 01:01:12 +0200

    add alert for localstorage limit

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 const todos = useTodos();
+const open = ref(true);
 const { lang, code, $t, setLangCode } = useLang();
 provide("todos", todos);
 provide("nls", {
@@ -8,11 +9,24 @@ provide("nls", {
   $t,
   setLangCode,
 });
+const closeAlert = (close) => {
+  open.value = close;
+};
 </script>
 <template>
   <UApp>
-    <div class="nst-app h-full">
-      <main class="h-full">
+    <div class="nst-app h-full flex flex-col justify-center items-center">
+      <UAlert
+        v-if="open"
+        v-motion-pop-visible
+        class="w-fit m-2 font-semibold text-center"
+        :description="$t('clearLocalstorageIfNeedMoreSpace')"
+        color="neutral"
+        variant="outline"
+        close
+        @update:open="closeAlert"
+      />
+      <main class="h-full w-full">
         <NuxtLayout>
           <NuxtPage />
         </NuxtLayout>
