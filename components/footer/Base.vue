4b2c9bf2fadbe04e21664064eb319ddd3d11c517
xunita <yayasopeguesoro@gmail.com>
2025-04-16 01:45:18 +0200

    add github reference

--- components/footer/Base.vue
+++ components/footer/Base.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 <template>
   <footer
-    class="nst-app-default-layout-footer w-full flex items-center justify-center p-4"
+    class="nst-app-default-layout-footer w-full flex space-x-2 items-center justify-center p-4"
   >
     <ULink
       target="_blank"
@@ -9,6 +9,14 @@
       to="https://www.linkedin.com/in/sopoude/"
       as="button"
       >Yaya Sopegue SORO</ULink
+    >
+    <USeparator orientation="vertical" class="h-3" />
+    <ULink
+      target="_blank"
+      class="text-xs"
+      to="https://github.com/xunita/notsmart"
+      as="button"
+      >Github</ULink
     >
   </footer>
 </template>
