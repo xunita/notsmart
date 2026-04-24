0c266e50a4837d5e598fb9dcb250531b5fd21327
Yaya Sopegue Soro <42295537+xunita@users.noreply.github.com>
2026-04-24 18:24:18 +0200

    Implement expandable task description feature
    Added functionality to toggle description expansion based on length.

--- components/body/task/taskparts/Description.vue
+++ components/body/task/taskparts/Description.vue
@@ -1,16 +1,27 @@
 <script setup lang="ts">
 const task = inject("task");
+const expanded = ref(false)
+
+const isLong = computed(() => {
+  // ~4 lines ≈ 300 chars, ajuste selon ta font-size/width
+  return (task.description?.length ?? 0) > 300
+})
 </script>
 <template>
-  <div class="todo-base-description flex flex-col space-y-1">
-    <div class="todo-base-description-content">
-      <UIcon name="circum:text-align-left" class="mr-1.5 size-5 shrink-0" />
-      <span class="text-sm line-clamp-3">{{ task.description }}</span>
-    </div>
-    <!-- <div class="todo-base-description-files flex items-center space-x-1.5">
-      <UIcon name="tabler:paperclip" class="size-5" />
-      <span class="text-xs font-semibold">2</span>
-    </div> -->
-  </div>
+  <div class="todo-base-description-content">
+  <UIcon name="circum:text-align-left" class="mr-1.5 size-5 shrink-0 mt-0.5" />
+  <span>
+    <span class="text-sm" :class="expanded ? '' : 'line-clamp-4'">
+      {{ task.description }}
+    </span>
+    <button
+      v-if="isLong"
+      class="text-xs text-primary-500 hover:text-primary-400 mt-1 block"
+      @click="expanded = !expanded"
+    >
+      {{ expanded ? 'Show less' : 'Show more' }}
+    </button>
+  </span>
+</div>
 </template>
 <style scoped></style>
